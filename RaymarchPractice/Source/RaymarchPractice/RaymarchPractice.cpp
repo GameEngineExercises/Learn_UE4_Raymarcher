3e158179d527f12e214f741c3c759784d87924b0
NUZEROVI <nuvmcdxliv@gmail.com>
2022-11-23 04:04:54 +0800

    Initial

---
+++ RaymarchPractice/Source/RaymarchPractice/RaymarchPractice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RaymarchPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RaymarchPractice, "RaymarchPractice" );
---
+++ RaymarchPractice/Source/RaymarchPractice/RaymarchPracticeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RaymarchPracticeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RAYMARCHPRACTICE_API ARaymarchPracticeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
